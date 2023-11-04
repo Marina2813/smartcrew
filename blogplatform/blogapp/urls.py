a007a937d5cb8deba95ffc25b5abbf6cb99bca0c
marina <marinaroseshaju123@gmail.com>
2023-11-05 01:45:36 +0530

    login and signup fit

--- blogplatform/blogapp/urls.py
+++ blogplatform/blogapp/urls.py
@@ -1,6 +1,9 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path(route= 'login', view= views.loginPageView, name='login'),
+    path(route= 'signup', view= views.signupPageView, name='signup'),
+    path(route= 'signout', view= views.signout, name='signout')
 ]
--- blogplatform/blogapp/views.py
+++ blogplatform/blogapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def loginPageView(request):
     return render(request=request, template_name = 'login.html',context = {})
+def signupPageView(request):
+    return render(request=request, template_name = 'signup.html',context = {})
+def signout(request):
+    pass
